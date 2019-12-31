edf3ca39b226c2bc14abd28c59bdd2d0814a9549
Krishna Mani <krishnamani314@gmail.com>
2019-12-30 23:19:04 -0800

    update readme, update example

--- README.md
+++ README.md
@@ -8,10 +8,11 @@ Pipes provide:
 - A protected queue of floats, intended to store sensor data, motor setpoints, etc.
 
 To access data from a pipe, one must: 
-1. Acquire the pipe using  `GenericPipe::acquirePipe()`
+1. Acquire the pipe using  `GenericPipe::acquirePipe()`/`GenericPipe::blockingAcquirePipe()`
 2. Access whatever data is needed using the relevant functions
 3. Release the pipe using `GenericPipe::releasePipe()`
 
-`GenericPipe::acquirePipe()` returns true if the pipe was acquired, and false when it is being used. This way, code can still be run if it is not dependent on the data from the pipe. The previous implementation using condition variables did not allow for this.
+`GenericPipe::acquirePipe()` returns true if the pipe was acquired, and false when it is being used. This way, code can still be run if it is not dependent on the data from the pipe. 
 
+For cases where an efficient method of blocking for a pipe acquire, `GenericPipe::blockingAcquirePipe()` puts the thread to sleep using C++11 condition variables until the pipe has been released.
 See `example.cpp` for a reference use case.
--- src/example.cpp
+++ src/example.cpp
@@ -4,24 +4,21 @@
 #include <chrono>
 
 void f1(GenericPipe* pipe) {
-	while(true) {
-		pipe->blockingAcquirePipe();
-		std::cout << "f1 got the pipe" << std::endl;
-		std::this_thread::sleep_for(std::chrono::seconds(5)); // To simulate some work being done
-		pipe->pushQueue(50);
-		pipe->releasePipe(); // The other thread can now get it
-		return;
-	}
+	pipe->blockingAcquirePipe();
+	std::cout << "f1 got the pipe" << std::endl;
+	std::this_thread::sleep_for(std::chrono::seconds(5)); // To simulate some work being done
+	pipe->pushQueue(50);
+	pipe->releasePipe(); // The other thread can now get it
+	return;
 }
 
 void f2(GenericPipe* pipe) {
-	while(true) { // This is called spinning, on the actual robot threads are scheduled
-		pipe->blockingAcquirePipe();
-		std::cout << "f2 got the pipe" << std::endl;
-		std::cout << "Value from queue: " << pipe->popQueue() << std::endl; // If this function happens before the first one, this would throw an error
-		pipe->releasePipe();
-		return;
-	}
+	// In this case, nothing can be done until the pipe has been acquired
+	pipe->blockingAcquirePipe();
+	std::cout << "f2 got the pipe" << std::endl;
+	std::cout << "Value from queue: " << pipe->popQueue() << std::endl; // If this function happens before the first one, this would throw an error
+	pipe->releasePipe();
+	return;
 }
 
 int main() {
